b727c83eb6db7b159d1993724c6d704bf2ae6093
calmi2505-tech <calmi2505@gmail.com>
2026-07-12 14:28:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function mostrarSistema(imagenSrc, nombre, descripcion, estructuras) {
 
     imagen.src = imagenSrc;
     titulo.textContent = nombre;
-    texto.textContent = descripcion;
+    texto.innerHTML = descripcion;
 
     lista.innerHTML = "";
 
@@ -15,9 +15,49 @@ function mostrarSistema(imagenSrc, nombre, descripcion, estructuras) {
 
         const li = document.createElement("li");
         li.textContent = item;
+        li.style.cursor = "pointer";
 
-        li.onclick = function () {
-            alert("Pulsaste: " + item);
+        li.onclick = () => {
+
+            switch(item){
+
+                case "🦴 Cráneo":
+
+                    titulo.textContent = "🦴 Cráneo";
+
+                    texto.innerHTML = `
+<b>Ubicación:</b> Cabeza.<br><br>
+
+<b>Función:</b> Protege el encéfalo y sostiene las estructuras de la cara.<br><br>
+
+<b>Articula con:</b> Mandíbula y primeras vértebras cervicales.
+`;
+
+                break;
+
+                case "🦴 Mandíbula":
+
+                    titulo.textContent = "🦴 Mandíbula";
+
+                    texto.innerHTML = `
+<b>Ubicación:</b> Parte inferior de la cabeza.<br><br>
+
+<b>Función:</b> Permite la masticación y soporta los dientes inferiores.<br><br>
+
+<b>Articula con:</b> Cráneo.
+`;
+
+                break;
+
+                default:
+
+                    titulo.textContent = item;
+
+                    texto.innerHTML =
+                    "La información anatómica de esta estructura estará disponible próximamente.";
+
+            }
+
         };
 
         lista.appendChild(li);
@@ -25,6 +65,24 @@ function mostrarSistema(imagenSrc, nombre, descripcion, estructuras) {
     });
 
 }
+
+document.getElementById("caballo").onclick = () => {
+
+    mostrarSistema(
+        "assets/images/caballo.png",
+        "Caballo",
+        "Vista externa del caballo.",
+        [
+            "Cabeza",
+            "Cuello",
+            "Tronco",
+            "Miembros anteriores",
+            "Miembros posteriores",
+            "Cola"
+        ]
+    );
+
+};
 
 document.getElementById("oseo").onclick = () => {
 
@@ -35,7 +93,81 @@ document.getElementById("oseo").onclick = () => {
         [
             "🦴 Cráneo",
             "🦴 Mandíbula",
-            "🦴 Vértebras cervicales"
+            "🦴 Vértebras cervicales",
+            "🦴 Escápula",
+            "🦴 Húmero",
+            "🦴 Radio",
+            "🦴 Carpo",
+            "🦴 Metacarpo",
+            "🦴 Pelvis",
+            "🦴 Fémur",
+            "🦴 Tibia",
+            "🦴 Tarso"
+        ]
+    );
+
+};
+
+document.getElementById("muscular").onclick = () => {
+
+    mostrarSistema(
+        "assets/images/musculos.png",
+        "Sistema Muscular",
+        "Principales músculos del caballo.",
+        [
+            "Trapecio",
+            "Braquiocefálico",
+            "Glúteo medio",
+            "Bíceps femoral"
+        ]
+    );
+
+};
+
+document.getElementById("organos").onclick = () => {
+
+    mostrarSistema(
+        "assets/images/organos.png",
+        "Órganos",
+        "Órganos internos del caballo.",
+        [
+            "Corazón",
+            "Pulmones",
+            "Hígado",
+            "Estómago",
+            "Intestino"
+        ]
+    );
+
+};
+
+document.getElementById("respiratorio").onclick = () => {
+
+    mostrarSistema(
+        "assets/images/respiratorio.png",
+        "Sistema Respiratorio",
+        "Órganos encargados de la respiración.",
+        [
+            "Nariz",
+            "Laringe",
+            "Tráquea",
+            "Pulmones"
+        ]
+    );
+
+};
+
+document.getElementById("nervioso").onclick = () => {
+
+    mostrarSistema(
+        "assets/images/nervioso.png",
+        "Sistema Nervioso",
+        "Sistema nervioso central y periférico.",
+        [
+            "Encéfalo",
+            "Médula espinal",
+            "Nervios craneales",
+            "Nervios espinales"
         ]
     );
 
