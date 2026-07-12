cbcc038a885d6e26ba8484b631df8d0eff5039c4
calmi2505-tech <calmi2505@gmail.com>
2026-07-12 14:15:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function mostrarSistema(imagenSrc, nombre, descripcion, estructuras) {
 
     imagen.src = imagenSrc;
     titulo.textContent = nombre;
-    texto.innerHTML = descripcion;
+    texto.textContent = descripcion;
 
     lista.innerHTML = "";
 
@@ -16,33 +16,8 @@ function mostrarSistema(imagenSrc, nombre, descripcion, estructuras) {
         const li = document.createElement("li");
         li.textContent = item;
 
-        li.style.cursor = "pointer";
-
-        li.onclick = () => {
-
-            if (item === "🦴 Cráneo") {
-
-                titulo.textContent = "🦴 Cráneo";
-
-                texto.innerHTML = `
-<b>Ubicación:</b> Cabeza.<br><br>
-
-<b>Función:</b> Protege el encéfalo y sostiene la cara del caballo.<br><br>
-
-<b>Articula con:</b> Mandíbula y primeras vértebras cervicales.
-`;
-
-            }
-
-            else {
-
-                titulo.textContent = item;
-
-                texto.innerHTML =
-                "La información anatómica de esta estructura estará disponible próximamente.";
-
-            }
-
+        li.onclick = function () {
+            alert("Pulsaste: " + item);
         };
 
         lista.appendChild(li);
@@ -50,24 +25,6 @@ function mostrarSistema(imagenSrc, nombre, descripcion, estructuras) {
     });
 
 }
-
-document.getElementById("caballo").onclick = () => {
-
-    mostrarSistema(
-        "assets/images/caballo.png",
-        "Caballo",
-        "Vista externa del caballo.",
-        [
-            "Cabeza",
-            "Cuello",
-            "Tronco",
-            "Miembros anteriores",
-            "Miembros posteriores",
-            "Cola"
-        ]
-    );
-
-};
 
 document.getElementById("oseo").onclick = () => {
 
@@ -78,81 +35,7 @@ document.getElementById("oseo").onclick = () => {
         [
             "🦴 Cráneo",
             "🦴 Mandíbula",
-            "🦴 Vértebras cervicales",
-            "🦴 Escápula",
-            "🦴 Húmero",
-            "🦴 Radio",
-            "🦴 Carpo",
-            "🦴 Metacarpo",
-            "🦴 Pelvis",
-            "🦴 Fémur",
-            "🦴 Tibia",
-            "🦴 Tarso"
-        ]
-    );
-
-};
-
-document.getElementById("muscular").onclick = () => {
-
-    mostrarSistema(
-        "assets/images/musculos.png",
-        "Sistema Muscular",
-        "Principales músculos del caballo.",
-        [
-            "Trapecio",
-            "Braquiocefálico",
-            "Glúteo medio",
-            "Bíceps femoral"
-        ]
-    );
-
-};
-
-document.getElementById("organos").onclick = () => {
-
-    mostrarSistema(
-        "assets/images/organos.png",
-        "Órganos",
-        "Órganos internos del caballo.",
-        [
-            "Corazón",
-            "Pulmones",
-            "Hígado",
-            "Estómago",
-            "Intestino"
-        ]
-    );
-
-};
-
-document.getElementById("respiratorio").onclick = () => {
-
-    mostrarSistema(
-        "assets/images/respiratorio.png",
-        "Sistema Respiratorio",
-        "Órganos encargados de la respiración.",
-        [
-            "Nariz",
-            "Laringe",
-            "Tráquea",
-            "Pulmones"
-        ]
-    );
-
-};
-
-document.getElementById("nervioso").onclick = () => {
-
-    mostrarSistema(
-        "assets/images/nervioso.png",
-        "Sistema Nervioso",
-        "Sistema nervioso central y periférico.",
-        [
-            "Encéfalo",
-            "Médula espinal",
-            "Nervios craneales",
-            "Nervios espinales"
+            "🦴 Vértebras cervicales"
         ]
     );
 
